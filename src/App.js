b864940e50ce408399a7b664d27aaf64f8f0d818
Crihojames <crihojames@gmail.com>
2023-11-16 12:24:44 +0100

    add to  exercise Counter3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import Count from './components/Counter'
 import Counter2 from './components/Counter2';
+import Counter3 from './components/Counter3';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Count/>
-    <Counter2/> 
+    {/* <Count/>
+    <Counter2/>  */}
+    <Counter3 />
     </div>
   );
 }
